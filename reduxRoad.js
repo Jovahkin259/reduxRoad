bb10e22d7bac782099e01ee99ad1937d4281b9c7
Joshua Astle <jovahkin259@gmail.com>
2021-05-10 19:44:14 +0100

    Reducer

--- reduxRoad.js
+++ reduxRoad.js
@@ -5,3 +5,57 @@ const initialWagonState = {
     days: 0,
     cash: 200
   }
+
+  const reducer = (state = initialWagonState, action) => {
+    switch(action.type) {
+      case 'gather': {
+        return {
+          ...state,
+          supplies: state.supplies + 15,
+          days: state.days + 1
+        }
+      }
+      case 'travel': {
+        if (state.supplies - (20 * action.payload) > 0) {
+          return state
+        }
+        return {
+          ...state,
+          supplies: state.supplies - (20 * action.payload),
+          distance: state.distance + (10 * action.payload),
+          days: state.days + action.payload
+        }
+      }
+      case 'tippedWagon': {
+        return {
+          ...state,
+          supplies: state.supplies - 30,
+          days: state.days + 1
+        }
+      }
+      case 'sell': {
+        return {
+          ...state,
+          supplies: state.supplies - 20,
+          cash: state.cash + 15
+        }
+      }
+      case 'buy': {
+        return {
+          ...state,
+          supplies: state.supplies + 25,
+          cash: state.cash - 15
+        }
+      }
+      case 'theft': {
+        return {
+          ...state,
+          cash: state.cash / 2
+        }
+      }
+      default: {
+        return state;
+      }
+    }
+  }
+  // End Setup
